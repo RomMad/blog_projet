25a5a171c3479f5acd7f96e6b7e27d0e4ae92ef1
RomMad <romain.madelaine@gmail.com>
2019-06-26 18:25:39 +0200

    Créé fichier index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php include("head.html"); ?>
+
+<body>
+
+    <?php include("header.html"); ?>
+
+    <div class="container">
+
+        <section class="row">
+
+
+        </section>
+
+
+    </div>
+
+    <?php include("scripts.html"); ?>
+
+</html>
