8d8e1a24efb2cccbbbd9152869c000eabe1d4153
RomMad <romain.madelaine@gmail.com>
2019-06-26 19:22:16 +0200

    Créé fichier connection_bdd.php

---
+++ connection_bdd.php
@@ -0,0 +1,13 @@
+<?php
+// Connexion à la base de données
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+?>
