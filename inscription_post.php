2fc94c43bb700967ce9624e90bce1ac00a76a875
RomMad <romain.madelaine@gmail.com>
2019-06-29 19:15:09 +0200

    Créé inscription_post.php

---
+++ inscription_post.php
@@ -0,0 +1,22 @@
+<?php include("connection_bdd.php"); ?>
+
+<?php
+// Vérification de la validité des informations
+if (isset($_POST['pseudo'], $_POST['email'], $_POST['password'])) {
+    $pseudo = htmlspecialchars($_POST['pseudo']);
+    $email = htmlspecialchars($_POST['email']);
+    $password_hash = password_hash(htmlspecialchars($_POST['password']), PASSWORD_DEFAULT); // Hachage du mot de passe
+    // Insert les données dans la table membres
+    $req = $bdd->prepare('INSERT INTO membres(pseudo, email, pass) VALUES(:pseudo, :email, :pass)');
+    $req->execute(array(
+        'pseudo' => $pseudo,
+        'email' => $email,
+        'pass' => $password_hash,
+        ));
+
+        $statusInscription = "Inscription réussie.";
+    }
+// Redirige vers page d'inscription
+header('Location: inscription_page.php');
+
+?>
