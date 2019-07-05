446edb4470336b5e9ec17dce9d93f9374164090b
RomMad <romain.madelaine@gmail.com>
2019-07-05 18:53:15 +0200

    Créé ajout article dans post.php

--- new_post.php
+++ new_post.php
@@ -1,35 +1,33 @@
 <?php 
+    session_start();
 
-session_start();
+    include("connection_bdd.php");
 
-include("connection_bdd.php");
+    var_dump($_POST);
 
-var_dump($_POST);
-// Vérification si informations dans variable POST
+    if (empty($_SESSION)) {
+        echo "Vous devez vous connecter pour écrire un article.";
+        // Redirige vers la page de connexion
+        header('Location: connection.php');
+    };
+    // Vérification si informations dans variable POST
+    if (!empty($_POST)) {
+        $post_author = htmlspecialchars($_SESSION['ID']);
+        $post_title = htmlspecialchars($_POST['post_title']);
+        $post_content = htmlspecialchars($_POST['post_content']);
+        $post_status = htmlspecialchars($_POST['post_status']);
 
-if (empty($_SESSION)) {
-    echo "Vous devez vous connecter pour écrire un article.";
-    // Redirige vers la page de connexion
-    header('Location: connection.php');
-};
-
-if (!empty($_POST)) {
-    $user_login = htmlspecialchars($_POST['user_login']);
-    $user_email = htmlspecialchars($_POST['user_email']);
-    $user_pass_hash = password_hash(htmlspecialchars($_POST['user_pass']), PASSWORD_DEFAULT); // Hachage du mot de passe
-    // Insert les données dans la table users
-    $req = $bdd->prepare('INSERT INTO users(user_login, user_email, user_pass) VALUES(:user_login, :user_email, :user_pass)');
-    $req->execute(array(
-        'user_login' => $user_login,
-        'user_email' => $user_email,
-        'user_pass' => $user_pass_hash,
-        ));
-
-        $statusInscription = "Inscription réussie.";
-    // Redirige vers page d'inscription
-    // header('Location: inscription_page.php');
-}
-
+        // Insert les données dans la table posts
+        $req = $bdd->prepare('INSERT INTO posts(post_author, post_title, post_content, post_status) 
+        VALUES(:post_author, :post_title, :post_content, :post_status)');
+        $req->execute(array(
+            'post_author' => $post_author,
+            'post_title' => $post_title,
+            'post_content' => $post_content,
+            'post_status' => $post_status
+            ));
+        $statusPost = "Article enregistré.";
+    };
 ?>
 
 <!DOCTYPE html>
@@ -45,30 +43,31 @@ if (!empty($_POST)) {
         <section id="post_form" class="row">
 
             <div class="col-sm-12 col-md-10 mx-auto">
-                <form class="">
+                <form action="new_post.php" method="post" class="">
                     <h2>Nouvel article </h2>
                     <div class="form-group">
-                        <label for="title">Titre</label>
-                        <input type="text" class="form-control" id="title">
+                        <label for="post-post_title">Titre</label>
+                        <input type="text" name="post_title" class="form-control" id="post_title">
                     </div>
                     <div class="form-group">
-                        <label for="content">Contenu</label>
-                        <textarea class="form-control" id="content" rows="10"></textarea>
+                        <label for="post_content">Contenu</label>
+                        <textarea name="post_content" class="form-control" id="post_content" rows="10"></textarea>
                     </div>
                     <div class="form-group col-sm-4 col-md-2">
-                        <label for="status">Statut</label>
-                        <select class="form-control" id="status">
-                            <option>Publier</option>
+                        <label for="post_status">Statut</label>
+                        <select name="post_status" class="form-control" id="post_status">
+                            <option>Publié</option>
                             <option>Brouillon</option>
                         </select>
                     </div>
                     <div class="form-group float-right">
-                                <input type="submit" value="Valider" id="validation" class="btn btn-primary shadow">
+                        <input type="submit" value="Enregistrer" id="save" class="btn btn-primary shadow">
+                    </div>
                 </form>
 
                 <?php  
-                if (isset($statusInscription)) {
-                    echo $statusInscription;
+                if (isset($statusPost)) {
+                    echo $statusPost;
                 };
             ?>
 
