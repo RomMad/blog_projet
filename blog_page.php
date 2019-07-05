83ffbcd2fe531b1ee6b73d908c1c58a207198bfa
RomMad <romain.madelaine@gmail.com>
2019-07-05 12:23:54 +0200

    Créé jointure tables posts et users

--- blog_page.php
+++ blog_page.php
@@ -16,22 +16,24 @@
 
             <?php
             // Récupère les derniers posts
-            $req = $bdd->query('SELECT ID, post_title, post_content, DATE_FORMAT(post_date_creation, \'%d/%m/%Y à %Hh%imn\') AS post_date_creation_fr 
-            FROM posts 
-            ORDER BY post_date_creation 
+            $req = $bdd->query('SELECT p.ID, p.post_title, p.post_author, u.user_login, p.post_content, DATE_FORMAT(p.post_date_creation, \'%d/%m/%Y à %Hh%imn\') AS post_date_creation_fr 
+            FROM posts p
+            LEFT JOIN users u
+            ON p.post_author = u.ID
+            ORDER BY p.post_date_creation 
             DESC LIMIT 0, 10');
 
             while ($data = $req->fetch())
             {
             ?>
             <div class="card">
-                <div class="card-header">
+                <div class="card-header bg-dark text-light">
                     <h3>
                         <?php echo htmlspecialchars($data['post_title']); ?>
                     </h3>
-                    <em>Le <?php echo $data['post_date_creation_fr']; ?></em>
+                    <em>Le <?php echo $data['post_date_creation_fr']; ?> par <a href=""> <?php echo $data['user_login']; ?> </a></em>
                 </div>
-                <div class="card-body">
+                <div class="card-body text-body">
                 <?php
                 // Affiche le contenu du post
                 echo nl2br(htmlspecialchars($data['post_content']));
