305bbab429d16f1c017c91b7671b8162c011f633
RomMad <romain.madelaine@gmail.com>
2019-07-05 10:26:39 +0200

    Créé fichier blog_page.php

---
+++ blog_page.php
@@ -0,0 +1,22 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<?php include("head.html"); ?>
+
+<body>
+
+    <?php include("header.php"); ?>
+
+    <div class="container">
+
+        <section id="inscription" class="row">
+
+            
+        </section>
+
+    </div>
+
+    <?php include("scripts.html"); ?>
+
+</html
