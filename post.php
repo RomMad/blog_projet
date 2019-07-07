c76a1ab95774ccfcacce163195c899d04d5b8d77
RomMad <romain.madelaine@gmail.com>
2019-07-07 19:14:06 +0200

    Ajout du formulaire pour écrire un commentaire sur un article

--- post.php
+++ post.php
@@ -2,9 +2,27 @@
 
 include("connection_bdd.php");
 
-var_dump($_GET);
+var_dump($_POST);    
+// Vérification si informations dans variable POST
+if (!empty($_POST)) {
+    $comment_content = htmlspecialchars($_POST['comment_content']);
+        // Ajoute le commentaire
+        $req = $bdd->prepare('INSERT INTO comments(id_post, comment_author, comment_content) 
+        VALUES(:id_post, :comment_author, :comment_content)');
+        $req->execute(array(
+            'id_post' => $_SESSION['post_ID'],
+            'comment_author' => $_SESSION['ID'],
+            'comment_content' => $_POST['comment_content']
+            ));
+};
 
-$post = htmlspecialchars($_GET['post']);
+var_dump($_GET);
+if (!empty($_GET)) {
+    $post = htmlspecialchars($_GET['post']);
+    $_SESSION['post_ID'] = $post;
+} else {
+    $post = $_SESSION['post_ID'];
+};
 
 // Récupère le post
 $req = $bdd->prepare('SELECT p.ID, p.post_title, p.post_author, u.user_login, p.post_content, DATE_FORMAT(p.post_date_creation, \'%d/%m/%Y à %H:%i\') AS post_date_creation_fr 
@@ -15,7 +33,7 @@ WHERE p.ID=?');
 $req->execute(array($post));
 $data = $req->fetch();
 
-// Récupération des commentaires
+// Récupère les commentaires
 $req = $bdd->prepare('SELECT u.user_login, c.comment_content, DATE_FORMAT(c.comment_date_creation, \'%d/%m/%Y à %H:%i\') AS date_comment_fr 
 FROM comments c
 LEFT JOIN users u
@@ -49,9 +67,23 @@ $req->execute(array($post));
                 <?= nl2br(htmlspecialchars($data['post_content'])) ?>
                 </div>
             </div>
-
-            <h2>Commentaires</h2>
-
+            <a href="edit_post.php?post=<?= $post ?>">Modifier l'article<a>
+            <br />
+            <!-- Formuulaire d'ajout d'un commentaire -->
+            <h2 class="h3">Nouveau commentaire</h2>
+            <div class="row">
+                <form action="post.php" method="post" class="col-sm-12 col-md-6">
+                    <div class="form-group">
+                        <label for="comment_content"></label>
+                        <textarea name="comment_content" class="form-control" id="comment_content" rows="4"></textarea>
+                    </div>
+                    <div class="form-group float-right">
+                        <input type="submit" value="Enregistrer" id="save" class="btn btn-primary shadow">
+                    </div>
+                </form>
+            </div>
+            <!-- Affiche les commentaires -->
+            <h2 class="h3">Commentaires</h2>
             <?php
             while ($data = $req->fetch())
             {
@@ -61,9 +93,6 @@ $req->execute(array($post));
             <?php
             }
             ?>
-
-            <br />
-            <a href="edit_post.php?post=<?= $post ?>">Modifier l'article<a>
         </section>
 
     </div>
