f6cb9d68c64e3f850a3ec375b82d494723afff81
RomMad <romain.madelaine@gmail.com>
2019-07-08 22:10:24 +0200

    Modif connection : vérifie si login déjà utilisé dans la base

--- inscription.php
+++ inscription.php
@@ -6,33 +6,50 @@
     var_dump($_POST);
     // Vérifie si informations dans variable POST
     if (!empty($_POST)) {
-        $user_login = htmlspecialchars($_POST['login']);
-        $user_name = htmlspecialchars($_POST['name']);
-        $user_surname = htmlspecialchars($_POST['surname']);
-        $user_email = htmlspecialchars($_POST['email']);
-        $user_birthdate = !empty($_POST['birthdate']) ? htmlspecialchars($_POST['birthdate']) : NULL;
-        $user_pass_hash = password_hash(htmlspecialchars($_POST['pass']), PASSWORD_DEFAULT); // Hachage du mot de passe
-        // Insert les données dans la table users
-        $req = $bdd->prepare('INSERT INTO users(user_login, user_email, user_name, user_surname, user_birthdate, user_pass) VALUES(:user_login, :user_email, :user_name, :user_surname, :user_birthdate, :user_pass)');
-        $req->execute(array(
-            'user_login' => $user_login,
-            'user_email' => $user_email,
-            'user_name' => $user_name,
-            'user_surname' => $user_surname,
-            'user_birthdate' => $user_birthdate,
-            'user_pass' => $user_pass_hash,
-            ));
-            // Récupère l'ID de l'utilisateur
-            $req = $bdd->prepare('SELECT ID FROM users WHERE user_login = ?');
-            $req->execute(array($user_login));
+        $login = htmlspecialchars($_POST["login"]);
+        $name = htmlspecialchars($_POST["name"]);
+        $surname = htmlspecialchars($_POST["surname"]);
+        $email = htmlspecialchars($_POST["email"]);
+        $birthdate = !empty($_POST["birthdate"]) ? htmlspecialchars($_POST["birthdate"]) : NULL;
+        $pass = htmlspecialchars($_POST["pass"]);
+        $pass_confirm = htmlspecialchars($_POST["pass_confirm"]);
+        if ($name) {
+            // Vérifie si le login est déjà utilisé
+            $req = $bdd->query("SELECT COUNT(*) as NbID FROM users WHERE user_login='$login'");
             $data = $req->fetch();
-            // Ajoute les infos de l'utilisateurs dans la Session
-            $_SESSION['ID'] = $data['ID'];
-            $_SESSION['user_login'] = $user_login;
-            $statusInscription = "Inscription réussie.";
-            ?> 
-            <meta http-equiv="refresh" content="2;url=index.php"/>
-            <?php 
+            if ($data["NbID"]>0) {
+                $infoInscription = "Ce login est déjà utilisé. Veuillez en utiliser un autre.";
+            } else {
+                // Vérifie si la confirmation du mot de passe est identique
+                if ($pass==$pass_confirm) {
+                    $pass_hash = password_hash($pass, PASSWORD_DEFAULT); // Hachage du mot de passe
+                // Insert les données dans la table users
+                $req = $bdd->prepare("INSERT INTO users(user_login, user_email, user_name, user_surname, user_birthdate, user_pass) VALUES(:user_login, :user_email, :user_name, :user_surname, :user_birthdate, :user_pass)");
+                $req->execute(array(
+                    "user_login" => $login,
+                    "user_email" => $email,
+                    "user_name" => $name,
+                    "user_surname" => $surname,
+                    "user_birthdate" => $birthdate,
+                    "user_pass" => $pass_hash,
+                    ));
+                    // Récupère l'ID de l'utilisateur
+                    $req = $bdd->query("SELECT ID FROM users WHERE user_login ='$login'");
+                    $data = $req->fetch();
+                    // Ajoute les infos de l"utilisateurs dans la Session
+                    $_SESSION["ID"] = $data["ID"];
+                    $_SESSION["user_login"] = $login;
+                    $infoInscription = "Inscription réussie.";
+                    ?> 
+                    <meta http-equiv="refresh" content="2;url=index.php"/>
+                    <?php             
+                } else {
+                    $infoInscription = "Mot de passe et confirmation différents.";
+                };
+            };            
+        } else {
+            $infoInscription = "Veuillez saisir un Login.";
+        };
     };
 ?>
 
@@ -47,12 +64,11 @@
     <div class="container">
 
         <section id="inscription" class="row">
-
             <div class="col-sm-10 col-md-8 col-lg-6 mx-auto">
 
-            <?= isset($statusInscription) ? $statusInscription : '' ?>
-                <br /> <br />
-                <form action="inscription.php" method="post" class="col-md-12 card shadow">
+            <?= isset($infoInscription) ? $infoInscription : "" ?>
+
+                <form action="inscription.php" method="post" class="col-md-12 card shadow mt-4">
                     <div class="form-group row">
                         <h3 class="h4 card-header col-md-12 h2 bg-light text-dark">Inscription</h3>
                     </div>
@@ -61,48 +77,43 @@
                             <div class="row">
                                 <label for="login" class="col-md-4 col-form-label">Login</label>
                                 <div class="col-md-8">
-                                    <input type="text" name="login" id="login" 
-                                        class="form-control"><br />
+                                    <input type="text" name="login" id="login" class="form-control mb-4" required>
                                 </div>
                             </div>
                             <div class="row">
                                 <label for="email" class="col-md-4 col-form-label">Adresse email</label>
                                 <div class="col-md-8">
-                                    <input type="email" name="email" id="email" 
-                                        class="form-control"><br />
+                                    <input type="email" name="email" id="email" class="form-control mb-4" required>
                                 </div>
                             </div>
                             <div class="row">
                                 <label for="name" class="col-md-4 col-form-label">Nom</label>
                                 <div class="col-md-8">
-                                    <input type="text" name="name" id="name" class="form-control"><br />
+                                    <input type="text" name="name" id="name" class="form-control mb-4">
                                 </div>
                             </div>
                             <div class="row">
                                 <label for="surname" class="col-md-4 col-form-label">Prénom</label>
                                 <div class="col-md-8">
-                                    <input type="text" name="surname" id="surname" class="form-control"><br />
+                                    <input type="text" name="surname" id="surname" class="form-control mb-4">
                                 </div>
                             </div>
                             <div class="row">
                                 <label for="birthdate" class="col-md-4 col-form-label">Date de naissance</label>
                                 <div class="col-md-5">
-                                    <input type="date" name="birthdate" id="birthdate" class="form-control"><br />
+                                    <input type="date" name="birthdate" id="birthdate" class="form-control mb-4">
                                 </div>
                             </div>                            
                             <div class="row">
                                 <label for="pass" class="col-md-4 col-form-label">Mot de passe</label>
-                                <div class="col-md-8">
-                                    <input type="password" name="pass" id="pass" 
-                                        class="form-control"><br />
+                                <div class="col-md-5">
+                                    <input type="password" name="pass" id="pass" class="form-control mb-4" required>
                                 </div>
                             </div>
                             <div class="row">
-                                <label for="pass-confirm" class="col-md-4 col-form-label">Confirmation mot de
-                                    passe</label>
-                                <div class="col-md-8">
-                                    <input type="password" name="pass-confirm" id="pass-confirm" 
-                                        class="form-control"><br />
+                                <label for="pass_confirm" class="col-md-4 col-form-label">Confirmation mot de passe</label>
+                                <div class="col-md-5">
+                                    <input type="password" name="pass_confirm" id="pass_confirm" class="form-control mb-4" required>
                                 </div>
                             </div>
                             <div class="form-group row">
