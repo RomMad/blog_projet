90d4d566b71328ba557aa2c41196c03e853fb432
RomMad <romain.madelaine@gmail.com>
2019-07-09 10:29:41 +0200

    Modif forme (gras et couleur)

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
 <footer class="bg-dark mt-5 py-4 px-4 text-white">
         <p>Ce site web est un blog réalisé dans le cadre d'une formation de développeur Web.</p>
-        <p>© Romain MADELAINE | <a href="https://romain-mad.fr" target="_blank" class="text-light">romain-mad.fr</a></p>
+        <p>© Romain MADELAINE | <a href="https://romain-mad.fr" target="_blank" class="text-info">romain-mad.fr</a></p>
 </footer>
