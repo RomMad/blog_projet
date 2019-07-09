156f937c00216af0a7d439cd3eb387813d8c6ae2
RomMad <romain.madelaine@gmail.com>
2019-07-09 10:16:46 +0200

    Créé fichier footer.php

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="row bg-secondary text-white">
+    <div class="col-md-12">
+        <p>Ce site web est un blog de démonstrationréalisée dans le cadre d'une formation de développeur Web. <br />
+            © Romain MADELAINE | <a href="https://romain-mad.fr" target="_blank"
+                class="text-light">romain-mad.fr</a></p>
+    </div>
+</footer>
