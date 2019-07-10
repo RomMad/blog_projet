306acac2896f70beaf03aceec3ce005047253bb8
RomMad <romain.madelaine@gmail.com>
2019-07-10 23:29:32 +0200

    Modif include -> require (bis

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
 
 <!DOCTYPE html>
 <html lang="fr">
-<?php include("head.html"); ?>
+<?php require("head.html"); ?>
 
 <body>
 
-    <?php include("header.php"); ?>
+    <?php require("header.php"); ?>
 
     <div class="container">
 
