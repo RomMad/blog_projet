b8f4913139b7ecbb6401772f5804931279b2a29b
RomMad <romain.madelaine@gmail.com>
2019-07-11 18:08:19 +0200

    Vérification si commentaire est vide + message d'information lors de l'envoi

--- post.php
+++ post.php
@@ -9,13 +9,24 @@
         if (isset($_SESSION["user_ID"])) {
             $user_ID = $_SESSION["user_ID"];
         } else {
-            $user_ID = 0;
+            $user_ID = NULL;
         };
 
         $content = htmlspecialchars($_POST["content"]);
         $name = htmlspecialchars($_POST["name"]);
+        $msgComment = "Le commentaire a été ajouté.";
+        $typeAlert = "success";
+        $validation = true;
 
-        // Ajoute le commentaire
+        // Vérifie si le commentaire est vide
+        if (empty($content)) {
+            $msgComment = "Le commentaire est vide.";
+            $typeAlert = "danger";
+            $validation = false;
+        };
+
+        // Ajoute le commentaire si le commentaire n'est pas vide
+        if ($validation) {
         $req = $bdd->prepare("INSERT INTO comments(id_post, user_ID, user_name, content) 
         VALUES(:id_post, :user_ID, :user_name, :content)");
         $req->execute(array(
@@ -24,6 +35,13 @@
             "user_name" => $name,
             "content" => $_POST["content"]
             ));
+        };
+
+        $_SESSION["flash"] = array(
+            "msg" => $msgComment,
+            "type" =>  $typeAlert
+        );
+
     };
 
     var_dump($_GET);
@@ -84,14 +102,30 @@
     <?php require("header.php"); ?>
 
     <div class="container">
+
         <section id="post">
+
+        <?php 
+            if (isset($_SESSION["flash"])) {
+                ?>
+                <div id="msg-profil" class="alert alert-<?= $_SESSION["flash"]["type"] ?> alert-dismissible fade show" role="alert">                     
+                    <?= $_SESSION["flash"]["msg"] ?>
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button> 
+                </div>
+                <?php
+                unset($_SESSION["flash"]);
+            };
+        ?>
+
             <div class="card">
                 <div class="card-header bg-dark text-light">
                     <h1><?= htmlspecialchars($data["title"]) ?></h1>
-                    <em>Créé le <?= $data["date_creation_fr"] ?> par <a class="text-info" href=""> <?= htmlspecialchars($data["login"]) ?> </a> et modifié le <?= $data["date_update_fr"] ?></em>
+                    <em>Créé le <?=  htmlspecialchars($data["date_creation_fr"]) ?> par <a class="text-info" href=""> <?= htmlspecialchars($data["login"]) ?> </a> et modifié le <?=  htmlspecialchars($data["date_update_fr"]) ?></em>
                     <?php
                     if (isset($_SESSION["user_ID"]) && $_SESSION["user_ID"]==$data["user_ID"]) { ?>
-                        <a class="text-info a-edit-post" href="edit_post.php?post=<?= $data["ID"] ?>"><span class="far fa-edit"> Modifier</a>
+                        <a class="text-info a-edit-post" href="edit_post.php?post=<?=  htmlspecialchars($data["ID"]) ?>"><span class="far fa-edit"> Modifier</a>
                     <?php }; ?>
                 </div>
                 <div class="card-body text-body">
@@ -147,7 +181,7 @@
                                     <?php                        
                                         if (isset($_SESSION["user_ID"]) && $_SESSION["user_ID"]==$data["user_ID"]) { ?>
                                             <div>
-                                                <a href="post.php?post=<?= isset($post_ID) ? $post_ID : "" ?>&comment=<?= $data["ID"] ?>&action=erase" onclick="if(window.confirm('Voulez-vous vraiment supprimer le commentaire ?', 'Demande de confirmation')){return true;}else{return false;}"><span class="fas fa-times text-danger"></span></a>
+                                                <a href="post.php?post=<?= isset($post_ID) ? $post_ID : "" ?>&comment=<?=  htmlspecialchars($data["ID"]) ?>&action=erase" onclick="if(window.confirm('Voulez-vous vraiment supprimer le commentaire ?', 'Demande de confirmation')){return true;}else{return false;}"><span class="fas fa-times text-danger"></span></a>
                                             </div>
                                         <?php
                                         };
