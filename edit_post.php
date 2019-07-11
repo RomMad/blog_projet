e20e7e8317853e9323317649328c53f07d4e6934
RomMad <romain.madelaine@gmail.com>
2019-07-11 18:46:08 +0200

    Modif contrôle si titre article vide + message d'information

--- edit_post.php
+++ edit_post.php
@@ -18,21 +18,46 @@
         $status = htmlspecialchars($_POST["status"]);
         $date_creation = htmlspecialchars($_POST["date_creation"]);
         $date_update = htmlspecialchars($_POST["date_update"]);
+        $typeAlert = "info";
+        $validation = true;
 
-        // Ajoute l'article si nouvel article
-        if (isset($_POST["save"]) && empty($_POST["post_ID"])) {
-            $req = $bdd->prepare("INSERT INTO posts(user_ID, user_login, title, content, status) 
-            VALUES(:user_ID, :user_login, :title, :content, :status)");
-            $req->execute(array(
-                "user_ID" => $user_ID,
-                "user_login" => $user_login,
-                "title" => $title,
-                "content" => $content,
-                "status" => $status
-                ));
-            $infoPost = "Article enregistré.";
+        // Vérifie si le titre est vide
+        if (empty($title)) {
+            $msgPost = "Le titre de l'article est vide.";
+            $typeAlert = "danger";
+            $validation = false;
+        };
+
+        // Ajoute ou modifie l'article si le titre n'est pas vide
+        if ($validation) {
+            // Ajoute l'article si nouvel article
+            if (isset($_POST["save"]) && empty($_POST["post_ID"])) {
+                $req = $bdd->prepare("INSERT INTO posts(user_ID, user_login, title, content, status) 
+                VALUES(:user_ID, :user_login, :title, :content, :status)");
+                $req->execute(array(
+                    "user_ID" => $user_ID,
+                    "user_login" => $user_login,
+                    "title" => $title,
+                    "content" => $content,
+                    "status" => $status
+                    ));
+                $msgPost = "L'article a été enregistré.";
+            };
+
+            // Met à jour l'article si article existant
+            if (isset($_POST["save"]) && !empty($_POST["post_ID"])) {
+                $req = $bdd->prepare("UPDATE posts SET title = :new_title, content = :new_content, status = :new_status, date_update = NOW() WHERE ID = :post_ID");
+                $req->execute(array(
+                    "new_title" => $title,
+                    "new_content" => $content,
+                    "new_status" => $status,
+                    "post_ID" => $post_ID
+                    ));     
+                $msgPost = "L'article a été modifié.";
+            };
+
             // Récupère l'article
-            $req = $bdd->prepare("SELECT p.ID, p.title, p.user_ID, u.login, p.content, p.status, DATE_FORMAT(p.date_creation, \"%d/%m/%Y %H:%i\") AS date_creation_fr, DATE_FORMAT(p.date_update, \"%d/%m/%Y %H:%i\") AS date_update_fr 
+            $req = $bdd->prepare("SELECT p.ID, p.user_ID, u.login, DATE_FORMAT(p.date_creation, \"%d/%m/%Y %H:%i\") AS date_creation_fr, DATE_FORMAT(p.date_update, \"%d/%m/%Y %H:%i\") AS date_update_fr 
             FROM posts p
             LEFT JOIN users u
             ON p.user_ID = u.ID
@@ -40,38 +65,28 @@
             ORDER BY p.ID DESC 
             LIMIT 0, 1");
             $req->execute(array($user_ID));
-            $data = $req->fetch();
+            $dataPost = $req->fetch();
     
-            $title = $data["title"];
-            $content = $data["content"];
-            $post_ID = $data["ID"];
-            $post_user_ID  = $data["login"];
-            $date_creation = $data["date_creation_fr"];
-            $date_update = $data["date_update_fr"];
-            $status = $data["status"];
-        };
-
-        // Met à jour l'article si article existant
-        if (isset($_POST["save"]) && !empty($_POST["post_ID"])) {
-            $req = $bdd->prepare("UPDATE posts SET title = :new_title, content = :new_content, status = :new_status, date_update = NOW() WHERE ID = :post_ID");
-            $req->execute(array(
-                "new_title" => $title,
-                "new_content" => $content,
-                "new_status" => $status,
-                "post_ID" => $post_ID
-                ));     
-            $infoPost = "Article modifié.";
-            date_default_timezone_set('Europe/Paris');
-            $date_update = date("d/m/Y H:i");
+            $post_ID = htmlspecialchars($dataPost["ID"]);
+            $post_user_ID  = htmlspecialchars($dataPost["login"]);
+            $date_creation = htmlspecialchars($dataPost["date_creation_fr"]);
+            $date_update = htmlspecialchars($dataPost["date_update_fr"]);
         };
 
         // Supprime l'article
         if (isset($_POST["erase"]) && !empty($_POST["post_ID"])) {
             $req = $bdd->prepare("DELETE FROM posts WHERE ID = ? ");
             $req->execute(array($post_ID));
-            $infoPost = "Article supprimé.";
+            $msgPost = "L'article a été supprimé.";
+            $typeAlert = "warning";
             header("Refresh: 2; url=blog.php");
         };
+
+        $_SESSION["flash"] = array(
+            "msg" => $msgPost,
+            "type" =>  $typeAlert
+        );
+
     };
 
     // 
@@ -85,15 +100,15 @@
         ON p.user_ID = u.ID
         WHERE p.ID=?");
         $req->execute(array($idPost));
-        $data = $req->fetch();
+        $dataPost = $req->fetch();
 
-        $title = $data["title"];
-        $content = $data["content"];
-        $post_ID = $data["ID"];
-        $post_user_ID  = $data["login"];
-        $date_creation = $data["date_creation_fr"];
-        $date_update = $data["date_update_fr"];
-        $status = $data["status"];
+        $title = $dataPost["title"];
+        $content = $dataPost["content"];
+        $post_ID = $dataPost["ID"];
+        $post_user_ID  = $dataPost["login"];
+        $date_creation = $dataPost["date_creation_fr"];
+        $date_update = $dataPost["date_update_fr"];
+        $status = $dataPost["status"];
     };
 
 ?>
@@ -110,19 +125,23 @@
 
         <section id="post_form" class="row">
             <div class="col-sm-12 col-md-10 mx-auto">
+
                 <form action="edit_post.php" method="post" class="">
+
                     <h2 class="mb-4">Edition d'article</h2>
-                    <?php  
-                    if (isset($infoPost)) {
-                    ?>
-                    <div id="info-edit-post" class="alert alert-success alert-dismissible fade show" role="alert">
-                    <?= $infoPost ?>
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
+
                     <?php 
-                    };
+                        if (isset($_SESSION["flash"])) {
+                            ?>
+                            <div id="msg-profil" class="alert alert-<?= $_SESSION["flash"]["type"] ?> alert-dismissible fade show" role="alert">                     
+                                <?= $_SESSION["flash"]["msg"] ?>
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button> 
+                            </div>
+                            <?php
+                            unset($_SESSION["flash"]);
+                        };
                     ?>
 
                     <div class="row">
