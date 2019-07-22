4238858d271e0e9d24a2b8a914d6696b5623debc
RomMad <romain.madelaine@gmail.com>
2019-07-22 10:57:59 +0200

    Modif flèches 'précédent' et 'suivant' dans pagination

--- nav_pagination.php
+++ nav_pagination.php
@@ -21,7 +21,7 @@
             <nav aria-label="Page navigation">
                 <ul class="pagination justify-content-end">
                     <li class="page-item <?= $prevPageLink ?>">
-                    <a class="page-link <?= $prevPageColorLink ?> font-weight-bold"href="<?= $linkPagination ?>?page=<?= $prevPage . $anchorPagination ?>" tabindex="-1" aria-disabled="true"><</a>
+                    <a class="page-link <?= $prevPageColorLink ?> font-weight-bold"href="<?= $linkPagination ?>?page=<?= $prevPage . $anchorPagination ?>" tabindex="-1" aria-disabled="true"><span aria-hidden="true">&laquo;</span></a>
                     </li>
                     <?php 
                         if ($page>2 && $nbPages>3) {
@@ -49,7 +49,7 @@
                         };
                     ?>
                         <li class="page-item <?= $nextPageLink ?>">
-                    <a class="page-link <?= $nextPageColorLink ?> font-weight-bold"" href="<?= $linkPagination ?>?page=<?= $nextPage . $anchorPagination ?>">></a>
+                    <a class="page-link <?= $nextPageColorLink ?> font-weight-bold"" href="<?= $linkPagination ?>?page=<?= $nextPage . $anchorPagination ?>"><span aria-hidden="true">&raquo;</span></a>
                     </li>
                 </ul>
             </nav>
