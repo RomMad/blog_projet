5620e5f0d41e02db854fb42c0ccca2c3e5e4b4c4
RomMad <romain.madelaine@gmail.com>
2019-07-27 00:29:12 +0200

    Modif correction filtre statut dans admin comments

--- admin_comments.php
+++ admin_comments.php
@@ -57,8 +57,8 @@
             );
         };
         // Enregistre le filtre
-        if (isset($_POST["filter_status"]) && $_POST["filter_status"] == "moderate") {
-            $filter = "c.status = 1";
+        if ($_POST["filter_status"] >= "0") {
+            $filter = "c.status = " . htmlspecialchars($_POST["filter_status"]);
         };
     };
 
--- header.php
+++ header.php
@@ -10,7 +10,7 @@ if (!empty($POST)) {
 
 <header id="header">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark my-3 py-3 shadow">
-        <a class="navbar-brand text-info" href="index.php">Jean Forteroche | Le blog</a>
+        <a class="navbar-brand text-info font-weight-bold" href="index.php">Jean Forteroche | Le blog</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
