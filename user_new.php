70c9e238b4b4a6ac3941edbff08407b8a7485e22
RomMad <romain.madelaine@gmail.com>
2019-07-29 23:06:44 +0200

    Ajout d'un fil d'Ariane dans user_new.php

--- user_new.php
+++ user_new.php
@@ -131,6 +131,14 @@
 
     <div class="container">
 
+        <nav aria-label="breadcrumb">
+                <ol class="breadcrumb bg-transparent mb-0">
+                    <li class="breadcrumb-item"><a href="index.php" class="text-blue">Accueil</a></li>
+                    <li class="breadcrumb-item"><a href="admin.php" class="text-blue">Administration</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Ajout d'un utilisateur</li>
+                </ol>
+        </nav>
+
         <section id="inscription" class="row">
             <div class="col-sm-10 col-md-8 col-lg-6 mx-auto">
 
