6fa7949ed31c30048b58a3f4c66803662e7770fd
RomMad <romain.madelaine@gmail.com>
2019-07-29 15:26:14 +0200

    Modif fichier see_more_comment.js

--- js/see_more_comment.js
+++ js/see_more_comment.js
@@ -1,22 +1,24 @@
 // Système pour afficher l'intégralité du commentaire et système de modification du commentaire
 let commentsElt = document.getElementById("comments"); // la section avec tous les commentaires
 let nbCommentsElt = commentsElt.getElementsByClassName("comment").length; // compte le nombre de commentaires
-let commentsElt2 = commentsElt.getElementsByClassName("comment");
+let commentsElt2 = document.getElementsByClassName("comment");
+let commentsContentElt = document.getElementsByClassName("comment-content");
+console.log(commentsContentElt.length);
 
 // Boucle pour chaque commentaire
 for (let i = 0; i < nbCommentsElt; i++) {
-    let rectCardBodyElt = commentsElt2[i].getBoundingClientRect();
-    let commentContentElt = commentsElt2[i].querySelector(".card-body .comment-content");
-    let commentFadeOutElt = commentsElt2[i].querySelector(".card-body .comment-fade-out");
-    let aEditCommentElt = commentsElt2[i].querySelector(".card .edit-comment");
-    let formEditCommentElt = commentsElt2[i].querySelector(".comment .form-edit-comment");
-    let cancelEditCommentElt = commentsElt2[i].querySelector(".comment .form-edit-comment .cancel-edit-comment");
+    let rectCommentContentElt = commentsContentElt[i].getBoundingClientRect();
+    let commentContentElt = commentsElt2[i].querySelector(".comment-content");
+    let commentFadeOutElt = commentsElt2[i].querySelector(".comment-fade-out");
+    let aEditCommentElt = commentsElt2[i].querySelector(".edit-comment");
+    let formEditCommentElt = commentsElt2[i].querySelector(".form-edit-comment");
+    let cancelEditCommentElt = commentsElt2[i].querySelector(".form-edit-comment .cancel-edit-comment");
 
     // commentContentElt.style.transition = "max-height 0.5s ease";
 
     // Masque le contenu du commentaire quand celui dépasse les 200px de hauteur
-    if (rectCardBodyElt.height > 150) {
-        commentContentElt.style.maxHeight = "150px";
+    if (rectCommentContentElt.height > 125) {
+        commentContentElt.style.maxHeight = "125px";
         commentContentElt.style.overflow = "hidden";
         commentContentElt.className = "comment-content";
         commentFadeOutElt.className = "comment-fade-out d-block";
@@ -25,7 +27,7 @@ for (let i = 0; i < nbCommentsElt; i++) {
     // Affiche la totalité de commentaire au clic le contenu du commentaire
     commentContentElt.addEventListener("click", function () {
         if (commentContentElt.style.maxHeight === "2000px") {
-            commentContentElt.style.maxHeight = "150px";
+            commentContentElt.style.maxHeight = "100px";
             commentContentElt.style.overflow = "hidden";
             commentContentElt.className = "comment-content";
             commentFadeOutElt.className = "comment-fade-out d-block";
