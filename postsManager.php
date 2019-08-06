3bc3dd7d6a31a8a7ea1d4d0afc26efe6cdda6cf3
RomMad <romain.madelaine@gmail.com>
2019-08-07 00:53:35 +0200

    Modif fonction getlist de postsManager (retourne '' seulement si existe)

--- postsManager.php
+++ postsManager.php
@@ -67,7 +67,9 @@ class PostsManager {
         while ($datas = $req->fetch()) {
             $posts[] = new Posts($datas);
         }
-        return $posts;
+        if (isset($posts)) {
+            return $posts;
+        }
     }
     // Méthode de mise à jour d'un article
     public function update(Posts $post) {
