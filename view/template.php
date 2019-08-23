514d1803b2849436b9c1dfdcd466d1648c4497e6
RomMad <romain.madelaine@gmail.com>
2019-08-23 11:25:09 +0200

    Modif de forme css

--- view/template.php
+++ view/template.php
@@ -153,7 +153,7 @@
         
     </main>
 
-    <footer class="bg-dark mt-3 py-4 px-4 text-white shadow">
+    <footer class="bg-dark p-4 text-white shadow">
         <p>Ce site web est un blog réalisé dans le cadre d'une formation de développeur Web.</p>
         <p>© Romain MADELAINE | <a href="https://romain-mad.fr" target="_blank" class="text-blue">romain-mad.fr</a></p>
     </footer>
