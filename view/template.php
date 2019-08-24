4ee912c1f449ebcdbfdc8aa01f3af8967f81b6b9
RomMad <romain.madelaine@gmail.com>
2019-08-24 19:46:26 +0200

    Ajout Logo dans l'en-tête du site

--- view/template.php
+++ view/template.php
@@ -65,7 +65,7 @@
         <header id="header">
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-3 py-3 shadow">
                 <a class="navbar-brand text-blue"
-                    href="blog"><?= isset($_SESSION["settings"]) ? $_SESSION["settings"]->blog_name() : "Jean Forteroche | Le blog" ?></a>
+                    href="blog"><img id="logo_blog" src="public/images/logo.ico"> <?= isset($_SESSION["settings"]) ? $_SESSION["settings"]->blog_name() : "Le blog" ?></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
